c739275307aaf64cf7db1694e0e3f77785bd28b8
Dima <proxortv@gmail.com>
2017-02-04 18:05:52 +0700

    ротейтер

---
+++ Project/Assets/SourceCode/Rotater.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotater : MonoBehaviour {
+
+    public float speed;
+    public Vector3 vector;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(vector * Time.deltaTime * speed);
+	}
+}
