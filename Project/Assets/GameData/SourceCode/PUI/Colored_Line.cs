548cd78d464591f8b3181f2ef826661b0809942b
AndreyEDD <andreyeddi@mail.ru>
2017-07-23 22:17:46 +0300

    Новое меню выхода, Апдейты по магазину

---
+++ Project/Assets/GameData/SourceCode/PUI/Colored_Line.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colored_Line : MonoBehaviour {
+
+	public Color Normal,Orange,Green;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
